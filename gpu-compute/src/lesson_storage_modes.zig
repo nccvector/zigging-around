c1db74f7e403d4b28110ea82ad81cdb6a42517ce
nccvector <fzn1020@gmail.com>
2026-01-25 20:06:43 +0900

    finish lesson storage mode

--- gpu-compute/src/lesson_storage_modes.zig
+++ gpu-compute/src/lesson_storage_modes.zig
@@ -3,8 +3,20 @@ const metal = @import("metal.zig");
 
 pub fn main() !void {
     const COUNT: usize = 1024;
+    const shader_source: [*:0]const u8 =
+        \\#include <metal_stdlib>
+        \\using namespace metal;
+        \\
+        \\kernel void multiply2(
+        \\    device float* input[[buffer(0)]],
+        \\    uint id [[thread_position_in_grid]]
+        \\) {
+        \\    input[id] *= 2;
+        \\}
+    ;
 
     const device = try metal.Device.create();
+    const pipeline = try device.createPipeline(shader_source, "multiply2");
 
     // Shared buffer
     const shared = try device.createBuffer(f32, COUNT, .{ .storage = .shared });
@@ -12,19 +24,34 @@ pub fn main() !void {
     // Map to cpu and write data
     const mapped = shared.getHostSlice().?;
     for (0..COUNT) |i| {
-        mapped[i] = @floatFromInt(42);
+        mapped[i] = @floatFromInt(12);
     }
 
     // Private buffer
     const private = try device.createBuffer(f32, COUNT, .{ .storage = .private });
 
-    // Copy data from shared buffer to private buffer
     var cmd = try device.createCommand();
     {
-        var enc = cmd.blitEncoder();
-        defer enc.end();
+        // Copy data host -> device
+        var blit_enc = cmd.blitEncoder();
+        blit_enc.copy(shared, private);
+        blit_enc.end();
 
-        enc.copy(shared, private);
+        var comp_enc = cmd.computeEncoder(pipeline, .{});
+        comp_enc.setBuffer(private, 0);
+        comp_enc.dispatch1d(COUNT);
+        comp_enc.end();
+
+        // Copy data device -> host
+        var blit_enc2 = cmd.blitEncoder();
+        blit_enc2.copy(private, shared);
+        blit_enc2.end();
+    }
+
+    device.submit(&cmd);
+
+    for (0..COUNT) |i| {
+        try std.testing.expectEqual(@as(f32, 24), mapped[i]);
     }
 
     std.debug.print("WOW", .{});
